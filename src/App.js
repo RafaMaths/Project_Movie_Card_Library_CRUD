0378523c74ce543abb16d3a5bf531653a2081df7
RafaMaths <rmathscoach@gmail.com>
2021-05-07 15:44:18 -0300

    fix request 1: DONE

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Route path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route component={ NotFound } />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Route path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route component={ NotFound } />
+    </BrowserRouter>
   );
 }
 
